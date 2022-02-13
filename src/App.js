9d883bca59c98ec94015bedf90fec15981bd4702
Firas-Setti <setti.firas@gmail.com>
2022-02-13 15:18:47 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Form';
-import Profile from './Components/Profile';
 
 function App() {
   return (
     <div className="App">
       <Login />
-      <Profile/>
     </div>
   );
 }
